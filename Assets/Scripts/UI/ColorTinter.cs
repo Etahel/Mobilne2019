e1259c9e749eb61373d926154ff274ee3902cd51
Michał <m.tegi@outlook.com>
2020-01-13 21:59:30 +0100

    menu rework

---
+++ Assets/Scripts/UI/ColorTinter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+public static class Utils
+{
+    public static Color ChangeColorBrightness(Color color, float correctionFactor)
+    {
+        float red = (float)color.r;
+        float green = (float)color.g;
+        float blue = (float)color.b;
+
+        if (correctionFactor < 0)
+        {
+            correctionFactor = 1 + correctionFactor;
+            red *= correctionFactor;
+            green *= correctionFactor;
+            blue *= correctionFactor;
+        }
+        else
+        {
+            red = (255 - red) * correctionFactor + red;
+            green = (255 - green) * correctionFactor + green;
+            blue = (255 - blue) * correctionFactor + blue;
+        }
+
+        return new Color(color.a, (int)red, (int)green, (int)blue);
+    }
+}
---
+++ Assets/Scripts/UI/SpriteButton.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SpriteButton : MonoBehaviour, IPointerClickHandler,
+                                  IPointerDownHandler, IPointerEnterHandler,
+                                  IPointerUpHandler, IPointerExitHandler
+{
+    public string SceneToLoadOnClick;
+    private Color color;
+
+    void Start()
+    {
+        //Attach Physics2DRaycaster to the Camera
+        Camera.main.gameObject.AddComponent<Physics2DRaycaster>();
+
+        addEventSystem();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+       // Debug.Log("Mouse Clicked!");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        SpriteRenderer render = GetComponent<SpriteRenderer>();
+        color = new Color(render.color.r, render.color.b, render.color.g);
+        render.color = new Color(render.color.r/2, render.color.b/2, render.color.g/2);
+        //render.color = Color.black;
+        Debug.Log("Mouse Down!");
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        SpriteRenderer render = GetComponent<SpriteRenderer>();
+        render.color = color;
+        //SceneManager.LoadScene(SceneToLoadOnClick);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+    }
+
+    //Add Event System to the Camera
+    void addEventSystem()
+    {
+        GameObject eventSystem = null;
+        GameObject tempObj = GameObject.Find("EventSystem");
+        if (tempObj == null)
+        {
+            eventSystem = new GameObject("EventSystem");
+            eventSystem.AddComponent<EventSystem>();
+            eventSystem.AddComponent<StandaloneInputModule>();
+        }
+        else
+        {
+            if ((tempObj.GetComponent<EventSystem>()) == null)
+            {
+                tempObj.AddComponent<EventSystem>();
+            }
+
+            if ((tempObj.GetComponent<StandaloneInputModule>()) == null)
+            {
+                tempObj.AddComponent<StandaloneInputModule>();
+            }
+        }
+    }
+
+}
